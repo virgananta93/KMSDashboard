c7e71b179b1c0ab62085e03905f2325f50e65f0e
Virgananta Nugraha <virgafallerie2@gmail.com>
2020-11-06 12:19:38 +0800

    tabs work

--- src/app/modules/analytics/analytics.component.ts
+++ src/app/modules/analytics/analytics.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.scss']
+  styleUrls: ['./analytics.component.scss'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class AnalyticsComponent implements OnInit {
 
