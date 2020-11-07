3045919fd053085443ae86b4b1db5d899e273822
Virgananta Nugraha <virgafallerie2@gmail.com>
2020-11-07 21:19:17 +0800

    registration start

--- src/app/modules/posts/posts.component.ts
+++ src/app/modules/posts/posts.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PostsComponent implements OnInit {
 
